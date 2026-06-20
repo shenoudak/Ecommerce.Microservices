1b02e1b94ae207c08f8b2b4b1b8e8cc80b39f2b6
shenoudak <shenouda0128992@gmail.com>
2026-06-20 16:37:15 +0300

    Add MongoConfiguration and Implement Product Repository

---
+++ Services/Catalog/Catalog.Core/Repositories/IProductBrandRepository.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductBrandRepository
+    {
+        Task<IEnumerable<ProductBrand>> GetAllProductBrands();
+    }
+}
---
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProductById(string productId);
+        Task<Product> AddProduct(Product product);   
+        Task UpdateProduct(Product product);
+        Task DeleteProduct(string productId);
+    }
+}
